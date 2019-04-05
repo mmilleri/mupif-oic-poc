0b6fd7317c2fe5823644bd48d6e5e8197df5438e
mmilleri <milleri@esteco.com>
2019-04-05 17:54:18 +0200

    Improved session management

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 
 from bottle import run, template, request, route, redirect, response
 from keycloak import KeycloakOpenID
-from keycloak.exceptions import KeycloakAuthenticationError
+from keycloak.exceptions import KeycloakAuthenticationError, KeycloakGetError
 
 
 def main():
@@ -13,13 +13,19 @@ def main():
 def get_slash():
     if is_authorization_redirect():
         code = request.query['code']
+        session_id = request.query['session_state']
         token = keycloak_openid.token(grant_type="authorization_code", code=code, redirect_uri="http://localhost:8080")
-        set_cookies_from_token(token)
 
+        save_session(session_id, token)
         redirect('/')
 
-    elif user_is_logged():
-        access_token = request.get_cookie('access_token')
+    elif is_user_logged():
+        try:
+            token = sessions[request.get_cookie('session_id')]
+            access_token = token['access_token']
+        except KeyError:
+            response.delete_cookie('session_id')
+            return redirect('/')
 
         try:
             userinfo = keycloak_openid.userinfo(access_token)
@@ -31,15 +37,18 @@ def get_slash():
         except KeycloakAuthenticationError:
             refresh_tokens()
             redirect('/')
+
     else:
         return template('main.html', login_url=login_url)
 
 
 @route('/logout')
 def logout():
-    refresh_token = request.get_cookie('refresh_token')
+    token = sessions[request.get_cookie('session_id')]
+    refresh_token = token['refresh_token']
     keycloak_openid.logout(refresh_token)
-    clear_cookies()
+
+    clear_session()
     redirect('/')
 
 
@@ -47,27 +56,38 @@ def is_authorization_redirect():
     return 'code' in request.query.keys()
 
 
-def user_is_logged():
-    return request.get_cookie('access_token')
+def is_user_logged():
+    return request.get_cookie('session_id')
 
 
 def refresh_tokens():
-    refresh_token = request.get_cookie('refresh_token')
-    token = keycloak_openid.refresh_token(refresh_token)
-    set_cookies_from_token(token)
+    session_id = request.get_cookie('session_id')
+    token = sessions[session_id]
+    refresh_token = token['refresh_token']
+
+    try:
+        new_token = keycloak_openid.refresh_token(refresh_token)
+    except KeycloakGetError:
+        print('Token expired. Login again.')
+        clear_session()
+        return redirect('/')
+
+    save_session(session_id, new_token)
+    print('Token refreshed')
 
 
-def set_cookies_from_token(token):
-    response.set_cookie('access_token', token['access_token'], max_age=token['expires_in'])
-    response.set_cookie('refresh_token', token['refresh_token'], max_age=token['refresh_expires_in'])
+def save_session(session_id, token):
+    sessions[session_id] = token
+    response.set_cookie('session_id', session_id)
 
 
-def clear_cookies():
-    response.delete_cookie('access_token')
-    response.delete_cookie('refresh_token')
+def clear_session():
+    response.delete_cookie('session_id')
+    del sessions[request.get_cookie('session_id')]
 
 
 if __name__ == '__main__':
+    sessions = {}
     keycloak_conf = json.loads(open('keycloak.json').read())
     keycloak_openid = KeycloakOpenID(server_url=keycloak_conf['auth-server-url'] + "/",
                                      realm_name=keycloak_conf['realm'],
